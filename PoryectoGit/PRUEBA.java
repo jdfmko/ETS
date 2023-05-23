3e92d8044effc7e01bc05756dedf8584360aabf8
Dani <133513523+jdfmko@users.noreply.github.com>
2023-05-23 14:26:04 +0200

    comentario prueba git

--- PoryectoGit/PRUEBA.java
+++ PoryectoGit/PRUEBA.java
@@ -9,4 +9,14 @@
  */
 public class PRUEBA {
      //prueba de git
+    
+        public int numero_mayor(int a, int b, int c) {
+        if (a > b && a > c) {
+            return a;
+        } else if (c > b) {
+            return c;
+        } else {
+            return b;
+        }
+    }
 }
